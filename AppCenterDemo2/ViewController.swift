bdfc4285af434de042620b363532fd4d761d9e9e
harungunes <h.gunes13@gmail.com>
2024-04-03 21:17:51 +0200

    feat: add text fields and analytics

--- AppCenterDemo2/ViewController.swift
+++ AppCenterDemo2/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import AppCenterCrashes
+import AppCenterAnalytics
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var age: UITextField!
+    @IBOutlet weak var retirementAge: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if Crashes.hasCrashedInLastSession {
+            let alert = UIAlertController(title: "Oops", message: "App crashed", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "It's cool", style: .default))
+            self.present(alert, animated: true)
+        }
+        
+        Analytics.trackEvent("navigate-to-app")
+    }
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+//        Crashes.generateTestCrash()
+        
+        let currentAge: Int? = Int(age.text!)
+        let plannedRetirementAge: Int? = Int(retirementAge.text!)
+        
+        let properties: [String: String]? = ["current age" : "\(String(currentAge!))",
+                                             "retirement age" : "\(String(plannedRetirementAge!))"]
+        
+        Analytics.trackEvent("press-button", withProperties: properties)
+    }
+    
 }
 
